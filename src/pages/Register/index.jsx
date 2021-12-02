45ba61b5891833526e667f93d4b53ea38096156c
Andry Pebrianto <andrypeb227@gmail.com>
2021-12-02 20:28:16 +0700

    Create the Register Form

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Register.module.css";
 
 export default function Register() {
+  const [formData, setFormData] = useState({
+    email: "",
+    displayName: "",
+    password: "",
+  });
+
+  const onChangeHandler = (e) => {
+    setFormData((val) => ({
+      ...val,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <form className={styles["register-form"]} onSubmit={onSubmitHandler}>
+      <h2>Register</h2>
+      <hr />
+      {/* Email */}
+      <label>
+        <span>Email:</span>
+        <input
+          type='email'
+          name='email'
+          onChange={onChangeHandler}
+          value={formData.email}
+        />
+      </label>
+      {/* Display Name */}
+      <label>
+        <span>Display Name:</span>
+        <input
+          type='text'
+          name='displayName'
+          onChange={onChangeHandler}
+          value={formData.displayName}
+        />
+      </label>
+      {/* Password */}
+      <label>
+        <span>Password:</span>
+        <input
+          type='password'
+          name='password'
+          onChange={onChangeHandler}
+          value={formData.password}
+        />
+      </label>
+      <button className='btn'>Login</button>
+    </form>
   );
 }
