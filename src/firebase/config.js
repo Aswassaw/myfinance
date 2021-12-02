5a8429001cbc9292b7398a836596835e92afc791
Andry Pebrianto <andrypeb227@gmail.com>
2021-12-02 20:58:58 +0700

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyAH0cN2Qqe0pdN3uN2iwJAQEPRxrCjSpzQ",
+  authDomain: "myfinance-cff2a.firebaseapp.com",
+  projectId: "myfinance-cff2a",
+  storageBucket: "myfinance-cff2a.appspot.com",
+  messagingSenderId: "516438316183",
+  appId: "1:516438316183:web:6fb6fa6ce4a9842763c5d2",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase Services
+const firestoreService = firebase.firestore();
+
+export { firestoreService };
