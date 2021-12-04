aed84f13aa406485ac631db9b1235bcdd861ccb4
Andry Pebrianto <andrypeb227@gmail.com>
2021-12-04 19:31:12 +0700

    Sedikit style untuk halaman landing

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import styles from "./Landing.module.css";
 
 export default function Landing() {
   return (
-    <div>
-      <h1>Landing</h1>
+    <div className={styles.landing}>
+      <h1>myFinance</h1>
+      <p>Solusi untuk menyimpan catatan pengeluaran anda dengan mudah.</p>
     </div>
   );
 }
