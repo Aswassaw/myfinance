a72e47453b0b3c2488a1177e24835fe7b93ff2cb
Andry Pebrianto <andrypeb227@gmail.com>
2021-12-04 10:06:24 +0700

    Creating a useCollection Hook

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { fbFirestore } from "../config/firebase";
+
+export default function useCollection(collection) {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const ref = fbFirestore.collection(collection);
+
+    const unsub = ref.onSnapshot(
+      (snapshot) => {
+        let results = [];
+        snapshot.docs.forEach((doc) => {
+          results.push({ ...doc.data(), id: doc.id });
+        });
+
+        // update state
+        setDocuments(results);
+        setError(null);
+      },
+      (err) => {
+        console.log(err);
+        setError("Cound Not Fetch the Data.");
+      }
+    );
+
+    // clean up
+    return () => {
+      unsub();
+    };
+  }, [collection]);
+
+  return { documents, error };
+}
